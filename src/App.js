611d1fcf59ba17b10b80158b92180d7cadc02059
CyberArbor <79295892+CyberArbor@users.noreply.github.com>
2022-04-05 19:45:58 -0700

    P2 ADR

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Cuadro1, {Cuadro2} from "./components/Cuadro1";
+import Saludo, {Bye} from "./components/Saludo";
+
+function App() {
+  return (
+    <>
+    <Bye/>
+    <Saludo/>
+    <Cuadro1/>
+    
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Saludo.js
@@ -0,0 +1,11 @@
+export default function Saludo(){
+    return(
+        <h1>quiubole</h1>
+    );
+}
+
+export function Bye(){
+    return(
+        <h1>Bye</h1>
+        );
+}
